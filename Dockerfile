44603fb4bec2a8f459a3c5930c2b3cfd9c2eb08a
Robert Lemke <robert@flownative.com>
2017-02-17 12:54:55 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM eu.gcr.io/flownative-beach/base:latest
+MAINTAINER Robert Lemke <robert@flownative.com>
+
+RUN groupadd -r -g 1000 beach && useradd -s /bin/bash -r -g beach -G beach -p "*" -u 1000 beach
+
+RUN apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62
+RUN echo "deb http://nginx.org/packages/mainline/ubuntu/ trusty nginx" >> /etc/apt/sources.list
+
+# Note: we need nginx-extras for the chunkin and more headers module and apache2-utils for the htpasswd command
+RUN apt-get update \
+    && apt-get install -y \
+        ca-certificates \
+        nginx-extras \
+        apache2-utils \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm /etc/nginx/sites-available/default \
+    && rm /etc/nginx/sites-enabled/default
+
+# forward request and error logs to docker log collector
+RUN ln -sf /dev/stdout /var/log/nginx/access.log
+RUN ln -sf /dev/stderr /var/log/nginx/error.log
+
+COPY service-nginx.sh /etc/service/nginx/run
+RUN chmod 755 /etc/service/nginx/run \
+    && chown root:root /etc/service/nginx/run
+COPY nginx.conf /etc/nginx/nginx.conf
+
+EXPOSE 80
---
+++ service-nginx.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Define defaults:
+export BEACH_CLUSTER_TYPE=${BEACH_CLUSTER_TYPE:-compose}
+
+export BEACH_APPLICATION_PATH=${BEACH_APPLICATION_PATH:-/application}
+export BEACH_APPLICATION_PATH=${BEACH_APPLICATION_PATH%/}
+
+export BEACH_FLOW_BASE_CONTEXT=${BEACH_FLOW_BASE_CONTEXT:-Production}
+export BEACH_FLOW_CONTEXT=${BEACH_FLOW_BASE_CONTEXT}
+
+export BEACH_PHP_FPM_HOST=${BEACH_PHP_FPM_HOST:-localhost}
+export BEACH_PHP_FPM_PORT=${BEACH_PHP_FPM_PORT:-9000}
+
+
+sudo -u www-data cat > /etc/nginx/sites-enabled/site.conf <<- EOM
+
+server {
+
+    listen *:80 default_server;
+
+    root ${BEACH_APPLICATION_PATH}/Web;
+
+    client_max_body_size 50M;
+    index index.php;
+
+    location ~ /\\. {
+        deny all;
+        access_log off;
+        log_not_found off;
+    }
+
+    try_files \$uri \$uri/ /index.php?\$args;
+
+    location ~ \\.php\$ {
+           include fastcgi_params;
+
+           fastcgi_pass ${BEACH_PHP_FPM_HOST}:${BEACH_PHP_FPM_PORT};
+           fastcgi_index index.php;
+
+           fastcgi_param FLOW_CONTEXT ${BEACH_FLOW_CONTEXT};
+           fastcgi_param FLOW_REWRITEURLS 1;
+           fastcgi_param FLOW_ROOTPATH ${BEACH_APPLICATION_PATH};
+
+           fastcgi_split_path_info ^(.+\\.php)(.*)\$;
+           fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+           fastcgi_param PATH_INFO \$fastcgi_path_info;
+    }
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$args;
+    }
+
+    location ~* \\.(jpg|jpeg|gif|css|png|js|ico|svg|woff|map)\$ {
+           access_log off;
+           expires max;
+    }
+}
+EOM
+
+exec /usr/sbin/nginx -g "daemon off;"
